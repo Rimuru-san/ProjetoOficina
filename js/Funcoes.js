c0418835dab78bc1a6ec08678f557abe73ae95ab
Yann Rodrigues Casanova <87773516+Rimuru-san@users.noreply.github.com>
2021-12-02 17:11:51 -0300

    Add files via upload

---
+++ js/Funcoes.js
@@ -0,0 +1,26 @@
+function PesquisarMarca(codigo){
+
+    codigo = codigo.value;
+$(function(){
+    $.getJSON("js/automoveis.json", function(data){
+        for(var i = 0; i < data['automoveis'].length; i++)
+        if (data['automoveis'][i]['MARCA'].match(codigo)){
+            $("#TableData").append("<tr>");
+            $("#TableData").append("<td scope='col'>" + data['automoveis'][i]['MARCA'] + "</td>");
+            $("#TableData").append("<td scope='col'>" + data['automoveis'][i]['CARRO'] + "</td>");
+            $("#TableData").append("<td scope='col'>" + data['automoveis'][i]['CAMBER_DIANTEIRO_MIN'] + "</td>");
+            $("#TableData").append("<td scope='col'>" + data['automoveis'][i]['CAMBER_DIANTEIRO_MAX'] + "</td>");
+            $("#TableData").append("<td scope='col'>" + data['automoveis'][i]['CASTER_DIANTEIRO_MIN'] + "</td>");
+            $("#TableData").append("<td scope='col'>" + data['automoveis'][i]['CASTER_DIANTEIRO_MAX'] + "</td>");
+            $("#TableData").append("<td scope='col'>" + data['automoveis'][i]['CONVERGENCIA_DIANTEIRA_MIN'] + "</td>");
+            $("#TableData").append("<td scope='col'>" + data['automoveis'][i]['CONVERGENCIA_DIANTEIRA_MAX'] + "</td>");
+            $("#TableData").append("<td scope='col'>" + data['automoveis'][i]['CAMBER_TRASEIRA_MIN'] + "</td>");
+            $("#TableData").append("<td scope='col'>" + data['automoveis'][i]['CAMBER_TRASEIRA_MAX'] + "</td>");
+            $("#TableData").append("<td scope='col'>" + data['automoveis'][i]['CONVERGENCIA_TRASEIRA_MIN'] + "</td>");
+            $("#TableData").append("<td scope='col'>" + data['automoveis'][i]['CONVERGENCIA_TRASEIRA_MAX'] + "</td>");
+            $("#TableData").append("<tr>");
+        }
+    
+    });
+});
+}
